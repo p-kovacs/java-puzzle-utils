62e05f99a526617b329406977bceaeb79a1246ef
Péter Kovács <kovacspeter84@gmail.com>
2024-12-20 14:43:04 +0100

    Merge InputUtils and RegexUtils into Utils

--- src/test/java/com/github/pkovacs/util/UtilsTest.java
+++ src/test/java/com/github/pkovacs/util/UtilsTest.java
@@ -7,19 +7,274 @@ import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.regex.MatchResult;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class UtilsTest extends Utils {
 
-    // ****************************** MATH UTILS ******************************
+    // **************************************** STRINGS AND TEXT FILES ****************************************
+
+    @Test
+    void testToLineBlocks() {
+        String input = "a\nb c d\ne\n\n\n\nf g\nh\n\ni j k";
+        var blocks = toLineBlocks(input);
+
+        assertEquals(3, blocks.size());
+        assertEquals(List.of("a", "b c d", "e"), blocks.get(0));
+        assertEquals(List.of("f g", "h"), blocks.get(1));
+        assertEquals(List.of("i j k"), blocks.get(2));
+
+        assertEquals(3, toLineBlocks(input + "\n").size());
+        assertEquals(3, toLineBlocks(input + "\n\n\n\n").size());
+
+        String inputWin = "a\r\nb c d\r\ne\r\n\r\nf g\r\nh\r\n\r\ni j k";
+        var blocks2 = toLineBlocks(inputWin);
+        assertEquals(blocks, blocks2);
+    }
+
+    @Test
+    void testParseIntegers() {
+        String input1 = "5 apples and 12 bananas. -42 is the opposite of 42.";
+        String input2 = "-1-2+3, 5-10, 6+-12. A23, B-34. [-100,+200]";
+        assertArrayEquals(new int[] { 5, 12, -42, 42 }, parseInts(input1));
+        assertArrayEquals(new long[] { 5, 12, -42, 42 }, parseLongs(input1));
+        assertArrayEquals(new int[] { -1, 2, 3, 5, 10, 6, -12, 23, 34, -100, 200 }, parseInts(input2));
+        assertArrayEquals(new long[] { -1, 2, 3, 5, 10, 6, -12, 23, 34, -100, 200 }, parseLongs(input2));
+    }
+
+    @Test
+    void testParseSingleIntegerFromChar() {
+        assertEquals(0, parseInt('0'));
+        assertEquals(5, parseInt('5'));
+        assertThrows(NumberFormatException.class, () -> parseInt('a'));
+        assertThrows(NumberFormatException.class, () -> parseInt('A'));
+
+        assertEquals(0, parseInt('0', 16));
+        assertEquals(5, parseInt('5', 16));
+        assertEquals(10, parseInt('a', 16));
+        assertEquals(10, parseInt('A', 16));
+        assertEquals(15, parseInt('f', 16));
+        assertEquals(15, parseInt('F', 16));
+        assertEquals(35, parseInt('z', 36));
+        assertEquals(35, parseInt('Z', 36));
+        assertThrows(NumberFormatException.class, () -> parseInt('z', 35));
+        assertThrows(NumberFormatException.class, () -> parseInt('Z', 35));
+    }
+
+    @Test
+    void testParseSingleIntegerFromString() {
+        assertEquals(42, parseInt("42"));
+        assertEquals(-123, parseInt("-123"));
+        assertThrows(NumberFormatException.class, () -> parseInt("1a"));
+        assertThrows(NumberFormatException.class, () -> parseInt("2A"));
+
+        assertEquals(16, parseInt("10", 16));
+        assertEquals(34, parseInt("42", 8));
+        assertEquals(170, parseInt("aa", 16));
+        assertEquals(255, parseInt("FF", 16));
+        assertEquals(46655, parseInt("zzz", 36));
+        assertEquals(46655, parseInt("ZZZ", 36));
+        assertThrows(NumberFormatException.class, () -> parseInt("zzz", 35));
+        assertThrows(NumberFormatException.class, () -> parseInt("ZZZ", 35));
+    }
+
+    @Test
+    void testParse() {
+        var values = parse("Product PID_4242X is ordered.", ".*PID_%d%c is %s[.]");
+
+        assertEquals("[4242, X, ordered]", values.toString());
+
+        assertEquals(3, values.size());
+        assertTrue(values.get(0).isLong());
+        assertEquals(4242, values.get(0).toInt());
+        assertEquals(4242L, values.get(0).toLong());
+        assertTrue(values.get(1).isChar());
+        assertEquals('X', values.get(1).toChar());
+        assertTrue(values.get(2).isString());
+        assertEquals("ordered", values.get(2).get());
+        assertEquals("ordered", values.get(2).toString());
+    }
+
+    @Test
+    void test() {
+        String s = "Hello World! I have 5 apples and 12 bananas. -42 is the opposite of 42.";
+        String regex = "-?\\d+";
+
+        assertFalse(matches(regex, s));
+        assertTrue(matches(".* " + regex + " is .*", s));
+
+        assertEquals("5", findFirst(regex, s));
+        assertEquals("5", findFirstMatch(regex, s).group());
+        assertEquals(20, findFirstMatch(regex, s).start());
+
+        assertThrows(NoSuchElementException.class, () -> findFirst("\\d[.]\\d", s));
+        assertThrows(NoSuchElementException.class, () -> findFirstMatch("\\d[.]\\d", s));
+
+        assertEquals(List.of("5", "12", "-42", "42"), findAll(regex, s));
+        assertEquals(List.of("5", "12", "-42", "42"),
+                findAllMatches(regex, s).stream().map(MatchResult::group).toList());
+        assertEquals(List.of(20, 33, 45, 68),
+                findAllMatches(regex, s).stream().map(MatchResult::start).toList());
+
+        assertEquals("Hello World! I have [+5] apples and [+12] bananas. [-42] is the opposite of [+42].",
+                replaceAll(regex, s,
+                        r -> "[" + (r.group().startsWith("-") ? "" : "+") + r.group() + "]"));
+    }
+
+    // **************************************** COLLECTIONS AND STREAMS ****************************************
+
+    @Test
+    void testCountMethods() {
+        assertEquals(3, count(List.of(1, 2, 3, 2, 1, 2, 3), 2));
+        assertEquals(2, count(List.of("a", "b", "c", "b"), "b"));
+        assertEquals(1, count(List.of("a", "b", "c", "b"), "c"));
+        assertEquals(0, count(List.of("a", "b", "c", "b"), "d"));
+        assertEquals(2, count(List.of("a", "bc", "d", "bc"), "bc"));
+        assertEquals(2, count(Arrays.asList("a", null, "d", null), null));
+
+        assertEquals(3, count("abcdcbab", 'b'));
+        assertEquals(2, count("abcdcbab", 'c'));
+        assertEquals(1, count("abcdcbab", 'd'));
+        assertEquals(0, count("abcdcbab", 'e'));
+    }
+
+    @Test
+    void testSetOperations() {
+        var c1 = setOf("hello".toCharArray());
+        var c2 = setOf("echo".toCharArray());
+        var c3 = listOf("love".toCharArray());
+        var c4 = listOf("old".toCharArray());
+
+        assertEquals(setOf('h', 'e', 'l', 'o', 'c'), unionOf(c1, c2));
+        assertEquals(setOf('h', 'e', 'l', 'o', 'v'), unionOf(c1, c3));
+        assertEquals(setOf('h', 'e', 'l', 'o', 'd'), unionOf(c1, c4));
+        assertEquals(setOf('e', 'c', 'h', 'o', 'l', 'v'), unionOf(c2.stream(), c3.stream()));
+        assertEquals(setOf('e', 'c', 'h', 'o', 'l', 'd'), unionOf(c2.stream(), c4.stream()));
+        assertEquals(setOf('l', 'o', 'v', 'e', 'd'), unionOf(c3.stream(), c4.stream()));
+        assertEquals(setOf('h', 'e', 'l', 'o', 'c', 'v'), unionOf(List.of(c1, c2, c3)));
+        assertEquals(setOf('h', 'e', 'l', 'o', 'v', 'd'), unionOf(List.of(c1, c3, c4)));
+        assertEquals(setOf('h', 'e', 'l', 'o', 'c', 'v', 'd'), unionOf(List.of(c1, c2, c3, c4)));
+
+        assertEquals(setOf('h', 'e', 'o'), intersectionOf(c1, c2));
+        assertEquals(setOf('e', 'l', 'o'), intersectionOf(c1, c3));
+        assertEquals(setOf('l', 'o'), intersectionOf(c1, c4));
+        assertEquals(setOf('e', 'o'), intersectionOf(c2.stream(), c3.stream()));
+        assertEquals(setOf('o'), intersectionOf(c2.stream(), c4.stream()));
+        assertEquals(setOf('l', 'o'), intersectionOf(c3.stream(), c4.stream()));
+        assertEquals(setOf('e', 'o'), intersectionOf(List.of(c1, c2, c3)));
+        assertEquals(setOf('l', 'o'), intersectionOf(List.of(c1, c3, c4)));
+        assertEquals(setOf('o'), intersectionOf(List.of(c1, c2, c3, c4)));
+    }
+
+    @Test
+    void testGenericParametersOfSetOperations() {
+        var a = List.of(listOf(1, 2), listOf(1, 2, 3));
+        var b = Set.of(new ArrayList<>(listOf(1)), new ArrayList<>(listOf(1, 2)));
+        var c = Set.of(new LinkedList<>(listOf(1)), new LinkedList<>(listOf(1, 2, 3)));
+
+        var union = Set.of(listOf(1), listOf(1, 2), listOf(1, 2, 3));
+        assertEquals(union, unionOf(a, b));
+        assertEquals(union, unionOf(b.stream(), a.stream()));
+        assertEquals(union, unionOf(a, c));
+        assertEquals(union, unionOf(c.stream(), a.stream()));
+        assertEquals(union, unionOf(b, c));
+        assertEquals(union, unionOf(c.stream(), b.stream()));
+        assertEquals(union, unionOf(List.of(a, b, c)));
+        assertEquals(union, unionOf(List.of(c, b, a)));
+
+        assertEquals(Set.of(listOf(1, 2)), intersectionOf(a, b));
+        assertEquals(Set.of(listOf(1, 2)), intersectionOf(b.stream(), a.stream()));
+        assertEquals(Set.of(listOf(1, 2, 3)), intersectionOf(a, c));
+        assertEquals(Set.of(listOf(1, 2, 3)), intersectionOf(c.stream(), a.stream()));
+        assertEquals(Set.of(listOf(1)), intersectionOf(b, c));
+        assertEquals(Set.of(listOf(1)), intersectionOf(c.stream(), b.stream()));
+        assertEquals(Set.of(), intersectionOf(List.of(a, b, c)));
+        assertEquals(Set.of(), intersectionOf(List.of(c, b, a)));
+    }
+
+    @Test
+    void testListSlicing() {
+        assertEquals(List.of(listOf(1, 2, 3), listOf(4, 5, 6)),
+                chunked(listOf(1, 2, 3, 4, 5, 6), 3).toList());
+        assertEquals(List.of(listOf(1, 2, 3), listOf(4, 5)),
+                chunked(listOf(1, 2, 3, 4, 5), 3).toList());
+        assertEquals(List.of(listOf(1, 2), listOf(3, 4), listOf(5)),
+                chunked(listOf(1, 2, 3, 4, 5), 2).toList());
+        assertEquals(List.of(listOf(1), listOf(2), listOf(3), listOf(4)),
+                chunked(listOf(1, 2, 3, 4), 1).toList());
+        assertEquals(List.of(listOf(1, 2, 3), listOf(4)),
+                chunked(listOf(1, 2, 3, 4), 3).toList());
+        assertEquals(List.of(listOf(1, 2, 3, 4)),
+                chunked(listOf(1, 2, 3, 4), 4).toList());
+        assertEquals(List.of(listOf(1, 2, 3, 4)),
+                chunked(listOf(1, 2, 3, 4), 5).toList());
+
+        assertThrows(IllegalArgumentException.class, () -> chunked(listOf(1, 2, 3), 0));
+        assertThrows(IllegalArgumentException.class, () -> chunked(listOf(1, 2, 3), -1));
+
+        assertEquals(List.of(listOf(1, 2, 3), listOf(2, 3, 4), listOf(3, 4, 5)),
+                windowed(listOf(1, 2, 3, 4, 5), 3).toList());
+        assertEquals(List.of(listOf(1, 2), listOf(2, 3), listOf(3, 4), listOf(4, 5)),
+                windowed(listOf(1, 2, 3, 4, 5), 2).toList());
+        assertEquals(List.of(listOf(1), listOf(2), listOf(3), listOf(4)),
+                windowed(listOf(1, 2, 3, 4), 1).toList());
+        assertEquals(List.of(listOf(1, 2, 3), listOf(2, 3, 4)),
+                windowed(listOf(1, 2, 3, 4), 3).toList());
+        assertEquals(List.of(listOf(1, 2, 3, 4)),
+                windowed(listOf(1, 2, 3, 4), 4).toList());
+        assertEquals(List.of(),
+                windowed(listOf(1, 2, 3, 4), 5).toList());
+
+        assertThrows(IllegalArgumentException.class, () -> windowed(listOf(1, 2, 3), 0));
+        assertThrows(IllegalArgumentException.class, () -> windowed(listOf(1, 2, 3), -1));
+    }
+
+    @Test
+    void testInverseMap() {
+        var map = Map.of('a', 1, 'b', 2, 'c', 3);
+
+        assertEquals(Map.of(1, 'a', 2, 'b', 3, 'c'), inverse(map));
+        assertEquals(map, inverse(inverse(map)));
+        assertEquals(Map.of(), inverse(Map.of()));
+    }
+
+    // **************************************** ARRAYS AND MATRICES ****************************************
+
+    @Test
+    void testDeepCopy() {
+        int[][] a = { { 0, 1, 2, 3, 4 }, {}, { Integer.MIN_VALUE, Integer.MAX_VALUE } };
+        int[][] b = deepCopy(a);
+
+        assertTrue(Arrays.deepEquals(a, b));
+        assertNotSame(a, b);
+        assertNotSame(a[0], b[0]);
+
+        long[][] c = { { 0, 1, 2, 3, 4 }, {}, { Long.MIN_VALUE, Long.MAX_VALUE } };
+        long[][] d = deepCopy(c);
+
+        assertTrue(Arrays.deepEquals(c, d));
+        assertNotSame(c, d);
+        assertNotSame(c[0], d[0]);
+
+        char[][] x = { "hello".toCharArray(), "".toCharArray(), "okay".toCharArray() };
+        char[][] y = deepCopy(x);
+
+        assertTrue(Arrays.deepEquals(x, y));
+        assertNotSame(x, y);
+        assertNotSame(x[0], y[0]);
+    }
+
+    // **************************************** MATH ****************************************
 
     @Test
     void testInts() {
@@ -205,149 +460,6 @@ class UtilsTest extends Utils {
         assertEquals(1234, lcm(List.of(1234)));
         assertEquals(1, lcm());
         assertEquals(1, lcm(List.of()));
-    }
-
-    // ****************************** COLLECTION AND STREAM UTILS ******************************
-
-    @Test
-    void testCountMethods() {
-        assertEquals(3, count(List.of(1, 2, 3, 2, 1, 2, 3), 2));
-        assertEquals(2, count(List.of("a", "b", "c", "b"), "b"));
-        assertEquals(1, count(List.of("a", "b", "c", "b"), "c"));
-        assertEquals(0, count(List.of("a", "b", "c", "b"), "d"));
-        assertEquals(2, count(List.of("a", "bc", "d", "bc"), "bc"));
-        assertEquals(2, count(Arrays.asList("a", null, "d", null), null));
-
-        assertEquals(3, count("abcdcbab", 'b'));
-        assertEquals(2, count("abcdcbab", 'c'));
-        assertEquals(1, count("abcdcbab", 'd'));
-        assertEquals(0, count("abcdcbab", 'e'));
-    }
-
-    @Test
-    void testSetOperations() {
-        var c1 = setOf("hello".toCharArray());
-        var c2 = setOf("echo".toCharArray());
-        var c3 = listOf("love".toCharArray());
-        var c4 = listOf("old".toCharArray());
-
-        assertEquals(setOf('h', 'e', 'l', 'o', 'c'), unionOf(c1, c2));
-        assertEquals(setOf('h', 'e', 'l', 'o', 'v'), unionOf(c1, c3));
-        assertEquals(setOf('h', 'e', 'l', 'o', 'd'), unionOf(c1, c4));
-        assertEquals(setOf('e', 'c', 'h', 'o', 'l', 'v'), unionOf(c2.stream(), c3.stream()));
-        assertEquals(setOf('e', 'c', 'h', 'o', 'l', 'd'), unionOf(c2.stream(), c4.stream()));
-        assertEquals(setOf('l', 'o', 'v', 'e', 'd'), unionOf(c3.stream(), c4.stream()));
-        assertEquals(setOf('h', 'e', 'l', 'o', 'c', 'v'), unionOf(List.of(c1, c2, c3)));
-        assertEquals(setOf('h', 'e', 'l', 'o', 'v', 'd'), unionOf(List.of(c1, c3, c4)));
-        assertEquals(setOf('h', 'e', 'l', 'o', 'c', 'v', 'd'), unionOf(List.of(c1, c2, c3, c4)));
-
-        assertEquals(setOf('h', 'e', 'o'), intersectionOf(c1, c2));
-        assertEquals(setOf('e', 'l', 'o'), intersectionOf(c1, c3));
-        assertEquals(setOf('l', 'o'), intersectionOf(c1, c4));
-        assertEquals(setOf('e', 'o'), intersectionOf(c2.stream(), c3.stream()));
-        assertEquals(setOf('o'), intersectionOf(c2.stream(), c4.stream()));
-        assertEquals(setOf('l', 'o'), intersectionOf(c3.stream(), c4.stream()));
-        assertEquals(setOf('e', 'o'), intersectionOf(List.of(c1, c2, c3)));
-        assertEquals(setOf('l', 'o'), intersectionOf(List.of(c1, c3, c4)));
-        assertEquals(setOf('o'), intersectionOf(List.of(c1, c2, c3, c4)));
-    }
-
-    @Test
-    void testGenericParametersOfSetOperations() {
-        var a = List.of(listOf(1, 2), listOf(1, 2, 3));
-        var b = Set.of(new ArrayList<>(listOf(1)), new ArrayList<>(listOf(1, 2)));
-        var c = Set.of(new LinkedList<>(listOf(1)), new LinkedList<>(listOf(1, 2, 3)));
-
-        var union = Set.of(listOf(1), listOf(1, 2), listOf(1, 2, 3));
-        assertEquals(union, unionOf(a, b));
-        assertEquals(union, unionOf(b.stream(), a.stream()));
-        assertEquals(union, unionOf(a, c));
-        assertEquals(union, unionOf(c.stream(), a.stream()));
-        assertEquals(union, unionOf(b, c));
-        assertEquals(union, unionOf(c.stream(), b.stream()));
-        assertEquals(union, unionOf(List.of(a, b, c)));
-        assertEquals(union, unionOf(List.of(c, b, a)));
-
-        assertEquals(Set.of(listOf(1, 2)), intersectionOf(a, b));
-        assertEquals(Set.of(listOf(1, 2)), intersectionOf(b.stream(), a.stream()));
-        assertEquals(Set.of(listOf(1, 2, 3)), intersectionOf(a, c));
-        assertEquals(Set.of(listOf(1, 2, 3)), intersectionOf(c.stream(), a.stream()));
-        assertEquals(Set.of(listOf(1)), intersectionOf(b, c));
-        assertEquals(Set.of(listOf(1)), intersectionOf(c.stream(), b.stream()));
-        assertEquals(Set.of(), intersectionOf(List.of(a, b, c)));
-        assertEquals(Set.of(), intersectionOf(List.of(c, b, a)));
-    }
-
-    @Test
-    void testListSlicing() {
-        assertEquals(List.of(listOf(1, 2, 3), listOf(4, 5, 6)),
-                chunked(listOf(1, 2, 3, 4, 5, 6), 3).toList());
-        assertEquals(List.of(listOf(1, 2, 3), listOf(4, 5)),
-                chunked(listOf(1, 2, 3, 4, 5), 3).toList());
-        assertEquals(List.of(listOf(1, 2), listOf(3, 4), listOf(5)),
-                chunked(listOf(1, 2, 3, 4, 5), 2).toList());
-        assertEquals(List.of(listOf(1), listOf(2), listOf(3), listOf(4)),
-                chunked(listOf(1, 2, 3, 4), 1).toList());
-        assertEquals(List.of(listOf(1, 2, 3), listOf(4)),
-                chunked(listOf(1, 2, 3, 4), 3).toList());
-        assertEquals(List.of(listOf(1, 2, 3, 4)),
-                chunked(listOf(1, 2, 3, 4), 4).toList());
-        assertEquals(List.of(listOf(1, 2, 3, 4)),
-                chunked(listOf(1, 2, 3, 4), 5).toList());
-
-        assertThrows(IllegalArgumentException.class, () -> chunked(listOf(1, 2, 3), 0));
-        assertThrows(IllegalArgumentException.class, () -> chunked(listOf(1, 2, 3), -1));
-
-        assertEquals(List.of(listOf(1, 2, 3), listOf(2, 3, 4), listOf(3, 4, 5)),
-                windowed(listOf(1, 2, 3, 4, 5), 3).toList());
-        assertEquals(List.of(listOf(1, 2), listOf(2, 3), listOf(3, 4), listOf(4, 5)),
-                windowed(listOf(1, 2, 3, 4, 5), 2).toList());
-        assertEquals(List.of(listOf(1), listOf(2), listOf(3), listOf(4)),
-                windowed(listOf(1, 2, 3, 4), 1).toList());
-        assertEquals(List.of(listOf(1, 2, 3), listOf(2, 3, 4)),
-                windowed(listOf(1, 2, 3, 4), 3).toList());
-        assertEquals(List.of(listOf(1, 2, 3, 4)),
-                windowed(listOf(1, 2, 3, 4), 4).toList());
-        assertEquals(List.of(),
-                windowed(listOf(1, 2, 3, 4), 5).toList());
-
-        assertThrows(IllegalArgumentException.class, () -> windowed(listOf(1, 2, 3), 0));
-        assertThrows(IllegalArgumentException.class, () -> windowed(listOf(1, 2, 3), -1));
-    }
-
-    @Test
-    void testInverseMap() {
-        var map = Map.of('a', 1, 'b', 2, 'c', 3);
-
-        assertEquals(Map.of(1, 'a', 2, 'b', 3, 'c'), inverse(map));
-        assertEquals(map, inverse(inverse(map)));
-        assertEquals(Map.of(), inverse(Map.of()));
-    }
-
-    // ****************************** ARRAY UTILS ******************************
-
-    @Test
-    void testDeepCopy() {
-        int[][] a = { { 0, 1, 2, 3, 4 }, {}, { Integer.MIN_VALUE, Integer.MAX_VALUE } };
-        int[][] b = deepCopy(a);
-
-        assertTrue(Arrays.deepEquals(a, b));
-        assertNotSame(a, b);
-        assertNotSame(a[0], b[0]);
-
-        long[][] c = { { 0, 1, 2, 3, 4 }, {}, { Long.MIN_VALUE, Long.MAX_VALUE } };
-        long[][] d = deepCopy(c);
-
-        assertTrue(Arrays.deepEquals(c, d));
-        assertNotSame(c, d);
-        assertNotSame(c[0], d[0]);
-
-        char[][] x = { "hello".toCharArray(), "".toCharArray(), "okay".toCharArray() };
-        char[][] y = deepCopy(x);
-
-        assertTrue(Arrays.deepEquals(x, y));
-        assertNotSame(x, y);
-        assertNotSame(x[0], y[0]);
     }
 
 }
